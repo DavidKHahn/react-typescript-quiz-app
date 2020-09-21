0635728de769c49d3db7f8d2b896e055afd43e50
David <dhahn123@gmail.com>
2020-09-20 20:38:06 -0700

    set up ternary for QuestionCard, questions (App.tsx)

--- react-quiz/src/App.tsx
+++ react-quiz/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Difficulty, fetchQuestions, QuestionState } from './API';
-// import QuestionCard from './components/QuestionCard';
+import QuestionCard from './components/QuestionCard';
 
 type AnswerObject = {
   question: string;
@@ -19,7 +19,7 @@ const App = () => {
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
 
-  console.log(fetchQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
+  console.log(questions)
 
   const startTrivia = async () => {
     setLoading(true);
@@ -46,17 +46,20 @@ const App = () => {
   return (
     <div className="App">
       <h1>REACT QUIZ</h1>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? (
       <button className="start" onClick={startTrivia}>Start</button>
-      <p className="score">Score:</p>
-      <p>Loading Questions...</p>
-      {/* <QuestionCard
+      ) : null}
+      {!gameOver ? <p className="score">Score:</p> : null}
+      {loading && <p>Loading Questions...</p>}
+      {!loading && !gameOver && (<QuestionCard
         questionNr={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      /> */}
+      />
+      )}
       <button className="next" onClick={nextQuestion}>Next Question</button>
     </div>
   );
